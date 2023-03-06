16d5dc38498a6347c2408a07d21b249e2814bd19
bduha184 <bduha_184@yahoo.co.jp>
2023-03-06 23:05:56 +0900

    ユーザー登録画面作成

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
 Route::get('/', [ArticleController::class,'index'])->name('articles.index');
+
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
