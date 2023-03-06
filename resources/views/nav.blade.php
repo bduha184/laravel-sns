9b6c72a01d077c443a26ea41808f5650304ce88d
bduha184 <bduha_184@yahoo.co.jp>
2023-03-07 08:45:08 +0900

    ルーティング設定 ArticleControllerのresource 被ってるarticles.indexを削除

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -12,7 +12,7 @@
 
         @guest
         <li class="nav-item">
-            <a class="nav-link" href="">ログイン</a>
+            <a class="nav-link" href="{{route('login')}}">ログイン</a>
         </li>
         @endguest
 
@@ -40,6 +40,7 @@
             </div>
         </li>
         <form id="logout-button" method="POST" action="{{route('logout')}}">
+            @csrf
         </form>
         <!-- Dropdown -->
 
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', [ArticleController::class,'index'])->name('articles.index');
-
-
+Route::resource('/articles',ArticleController::class)->except(['index']);
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
