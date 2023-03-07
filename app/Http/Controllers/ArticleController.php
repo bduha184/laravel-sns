0c6cf24672fd9662e93a31262509627fb0642382
bduha184 <bduha_184@yahoo.co.jp>
2023-03-07 22:04:05 +0900

    記事削除機能追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,21 +13,35 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::latest()->get();
-        return view('articles.index',compact('articles'));
+        return view('articles.index', compact('articles'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('articles.create');
     }
 
-    public function store(ArticleRequest $request,Article $article){
+    public function store(ArticleRequest $request, Article $article)
+    {
         $article = fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
     }
 
-    public function edit(Article $article){
-        return view('articles.edit',compact('article'));
+    public function edit(Article $article)
+    {
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article){
+        $article->delete();
+        return redirect()->route('articles.index');
     }
 }
