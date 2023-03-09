4544a585eaadfa47cef084fffadb0b46b40de849
bduha184 <bduha_184@yahoo.co.jp>
2023-03-09 22:13:43 +0900

    vue3-tags-input タグ入力欄表示

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,12 @@ import "./bootstrap";
 import "../css/app.css";
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import ArticleLike from "./components/ArticleLike.vue";
+import ArticleTagsInput from "./components/ArticleTagsInput.vue";
 
 const app = createApp({
     components:{
-        ArticleLike
+        ArticleLike,
+        ArticleTagsInput
     }
 
 });
