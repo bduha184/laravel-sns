28089499e9d98acf30b1d3e70cec5ca710ff0039
bduha184 <bduha_184@yahoo.co.jp>
2023-03-11 18:12:12 +0900

    フォローボタン設置 自身以外のアカウントの記事をクリックしたらフォローボタン表示

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,13 @@ import "../css/app.css";
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import ArticleLike from "./components/ArticleLike.vue";
 import ArticleTagsInput from "./components/ArticleTagsInput.vue";
+import FollowButton from "./components/FollowButton.vue";
 
 const app = createApp({
     components:{
         ArticleLike,
-        ArticleTagsInput
+        ArticleTagsInput,
+        FollowButton
     }
 
 });
