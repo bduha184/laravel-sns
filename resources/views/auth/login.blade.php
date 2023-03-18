4298d074e051b0461e0c1db1ac57fe0cb93025f1
bduha184 <bduha_184@yahoo.co.jp>
2023-03-18 09:27:57 +0900

    パスワード再設定画面作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <div class="card mt-3">
                     <div class="card-body text-center">
                         <h2 class="h3 card-title text-center mt-2">ログイン</h2>
-                        <a href="{{route('login.{provider}',['provider'=>'google'])}}"class="btn btn-block btn-danger">
+                        <a href="{{ route('login.{provider}', ['provider' => 'google']) }}"class="btn btn-block btn-danger">
                             <i class="fab fa-google mr-1"></i>Googleでログイン
                         </a>
                         @include('error_card_list')
@@ -32,6 +32,10 @@
                                 {{-- ここから --}}
                                 <input type="hidden" name="remember" id="remember" value="on">
                                 {{-- ここまで --}}
+                                <div class="text-left">
+                                    <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                                </div>
+
 
                                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
